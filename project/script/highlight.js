852532c591724b4a7734baffcfb2be9c81b61ecf
QAJamesClive <Jamesclive@academytrainee.com>
2018-08-16 09:51:31 +0100

    HighlighsJS

--- project/script/highlight.js
+++ project/script/highlight.js
@@ -1,5 +1,5 @@
 function hideSection(section){
-    document.getElementById(section+"Content").style.display="none";
+    document.getElementById(section+"Content").style.ma ="0";
     document.getElementById(section+"Show").style.display="inline"
     document.getElementById(section+"Hide").style.display="none"
     document.getElementById(section+"Show").style.textAlign="center";
@@ -9,4 +9,17 @@ function showSection(section){
     document.getElementById(section+"Hide").style.display="inline"
     document.getElementById(section+"Show").style.display="none"
     document.getElementById(section+"Hide").style.textAlign="center";
+}
+
+var highlights = [
+    { title: 'Item 1', image: "img/gig.jpeg", description: "A description" },
+    { title: 'Item 2', image: "img/gig.jpeg", description: "Another decription" },
+];
+
+for(i=0;i<highlights.length;i++){
+    highlightContent = document.getElementById('highlightContent');
+    highlightSection = document.createElement('div');
+    highlightSection.innerHTML = "<h2>" + highlights[i].title + "</h2><img src='" + highlights[i].image + "'><p>" + highlights[i].description + "</p>";
+    highlightSection.id = "Item";
+    highlightContent.appendChild(highlightSection);
 }
